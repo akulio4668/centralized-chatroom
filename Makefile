f385af67208ed365f1f76b790a4e2046e14ed177
Akul Joshi <akulj2@illinois.edu>
2021-03-06 03:33:56 +0000

    Pushing current ncurses work

--- Makefile
+++ Makefile
@@ -9,7 +9,7 @@ server:
 	$(CC) toy_server.c -o toy_server
 
 gui:
-	$(CC) curses_env.c -o curses_env -lncurses
+	$(CC) -Wall curses_env.c -o curses_env -lncurses
 
 clean:
 	rm toy_server toy_client curses_env
--- curses_env.c
+++ curses_env.c
@@ -1,7 +1,46 @@
 #include <curses.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    initscr();
+    long iter;
+    int yi, xi;
+    int y[3], x[3];
+    int index;
+    int maxlines, maxcols;
 
-    return 0;
+    initscr();
+    cbreak();
+    noecho();
+    
+    clear();
+
+    maxlines = LINES - 1;
+    maxcols = COLS - 1;
+
+    y[0] = 0;
+    x[0] = 0;
+
+    y[1] = maxlines;
+    x[1] = maxcols / 2;
+
+    y[2] = 0;
+    x[2] = maxcols;
+
+    mvaddch(y[0], x[0], '0');
+    mvaddch(y[1], x[1], '1');
+    mvaddch(y[2], x[2], '2');
+
+    yi = maxlines / 2;
+    xi = maxcols / 2;
+
+    mvaddch(yi, xi, '.');
+
+    mvaddstr(maxlines, 0, "Press any key to quit");
+
+    refresh();
+
+    getch();
+    endwin();
+
+    exit(0);
 }
