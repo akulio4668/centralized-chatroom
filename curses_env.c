9f81593262c3d276db64c573c68b52d6b5016f81
Akul Joshi <akulj2@illinois.edu>
2021-03-10 02:24:49 +0000

    Created a demo ncurses program from tutorial

--- curses_env.c
+++ curses_env.c
@@ -1,6 +1,8 @@
 #include <curses.h>
 #include <stdlib.h>
 
+#define ITERMAX 10000
+
 int main(int argc, char **argv) {
     long iter;
     int yi, xi;
@@ -30,10 +32,21 @@ int main(int argc, char **argv) {
     mvaddch(y[1], x[1], '1');
     mvaddch(y[2], x[2], '2');
 
-    yi = maxlines / 2;
-    xi = maxcols / 2;
+    yi = random() % (maxlines + 1);
+    xi = random() % (maxcols + 1);
 
     mvaddch(yi, xi, '.');
+
+    for (iter = 0; iter < ITERMAX; iter++) {
+        index = random() % 3;
+
+        yi = (yi + y[index]) / 2;
+        xi = (xi + x[index]) / 2;
+
+        mvaddch(yi, xi, '*');
+        refresh();
+    }
+
 
     mvaddstr(maxlines, 0, "Press any key to quit");
 
